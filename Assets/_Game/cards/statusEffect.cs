e5d6ca4519b8bc7566870430cbe5ec35594fb630
adzmunshi <158168233+adzmunshi@users.noreply.github.com>
2025-10-21 15:22:44 +0100

    base class for all status effects

---
+++ Assets/_Game/cards/statusEffect.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public abstract class StatusEffect : ScriptableObject
+{
+    public string effectName;
+    public float duration;
+
+    public virtual void OnApply(Character target) { }
+    public virtual void OnRemove(Character target) { }
+    public virtual void OnTurnStart(Character target) { }
+
+    public virtual int ModifyOutgoingDamage(int damage) => damage;
+    public virtual int ModifyIncomingDamage(int damage) => damage;
+}
