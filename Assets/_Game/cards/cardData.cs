288b73d5c4614a12fa1e13e60cf2a6be40d1c185
adzmunshi <158168233+adzmunshi@users.noreply.github.com>
2025-10-21 15:08:00 +0100

    created cardData

---
+++ Assets/_Game/cards/cardData.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCard", menuName = "Card")]
+public class cardData : ScriptableObject
+{
+    public string cardName;
+    public Sprite artwork;
+    public CardType type;
+    public int attack;
+    public StatusEffect statusEffect;
+    public string description;
+}
+
+public enum CardType
+{
+    Attack,
+    Defense,
+    Buff,
+    Heal
+}   
